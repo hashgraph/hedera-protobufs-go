5a3de4d70195070672654d9ccf59eab329b161fe
Ryan Leckey <leckey.ryan@gmail.com>
2021-07-08 22:58:56 -0700

    Setup example to query the account balance

---
+++ examples/get-account-balance/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashgraph/hedera-protobufs-go/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// https://docs.hedera.com/guides/testnet/testnet-nodes#testnet-nodes
+	conn, err := grpc.Dial("0.testnet.hedera.com:50211", grpc.WithInsecure())
+
+	if err != nil {
+		panic(err)
+	}
+
+	cryptoClient := services.NewCryptoServiceClient(conn)
+
+	// https://github.com/hashgraph/hedera-protobufs/blob/main/services/CryptoGetAccountBalance.proto#L35
+	getBalanceQuery := services.Query{
+		Query: &services.Query_CryptogetAccountBalance{
+			CryptogetAccountBalance: &services.CryptoGetAccountBalanceQuery{
+				BalanceSource: &services.CryptoGetAccountBalanceQuery_AccountID{
+					AccountID: &services.AccountID{
+						AccountNum: 1001,
+					},
+				},
+			},
+		},
+	}
+
+	// https://github.com/hashgraph/hedera-protobufs/blob/main/services/CryptoService.proto#L52
+	response, err := cryptoClient.CryptoGetBalance(context.TODO(), &getBalanceQuery)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// https://github.com/hashgraph/hedera-protobufs/blob/main/services/CryptoGetAccountBalance.proto#L47
+	getBalanceResponse := response.GetCryptogetAccountBalance()
+
+	fmt.Printf("balance = %v tℏ\n", getBalanceResponse.Balance)
+}
---
+++ examples/go.mod
@@ -0,0 +1,10 @@
+module github.com/hashgraph/hedera-protobufs-go/examples
+
+go 1.15
+
+require (
+	github.com/hashgraph/hedera-protobufs-go v0.1.0
+	google.golang.org/grpc v1.39.0
+)
+
+replace github.com/hashgraph/hedera-protobufs-go => ../
